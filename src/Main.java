87b4422cc9b2e9a835864b870b496b229267a06f
User <madilovamairamkan@gmail.com>
2023-02-15 17:16:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(9);
+        arrayList.add(10);
+        arrayList.add(11);
+        arrayList.add(12);
+        arrayList.add(13);
+        arrayList.add(14);
+        arrayList.add(15);
+        arrayList.add(16);
+        arrayList.add(17);
+        arrayList.add(21);
+        arrayList.add(36);
+        arrayList.add(42);
+        arrayList.add(54);
+        arrayList.add(63);
+        arrayList.add(71);
+        arrayList.add(85);
+        arrayList.add(97);
+        arrayList.add(28);
+        arrayList.add(82);
+        arrayList.add(76);
+        arrayList.add(94);
+        arrayList.add(33);
+        arrayList.add(26);
+        arrayList.add(58);
+        System.out.println(arrayList);
+
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+        arrayList.add(even.size());
+        arrayList.add(odd.size() );
+        for (int i = 1; i < 58; i++) {
+            if (i % 2 == 0) {
+
+
+                System.out.println(i);
+            }
+
+            }
+
+        }
+
+         //  System.out.println("вывести четные числа");
+
+       // System.out.println("вывести нечетные числа");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
